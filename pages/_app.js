96d8da34030379e98d5755513cc7346f8c680684
fahim2204 <fahimfaisal1998@gmail.com>
2022-12-21 02:18:41 +0600

    Done Some Form

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import {useEffect} from "react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  return(
+    <>
+    <Component {...pageProps} />
+    </>
+  ) 
 }
+
+export default MyApp
